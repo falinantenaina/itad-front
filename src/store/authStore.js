98b16ca02b93d7ea93dff7760da36b355528b382
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-12-21 22:00:19 +0300

    Store: auth store done

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,35 +2,66 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import api from "../lib/axios";
 
-
 export const useAuthStore = create(
-    persist(
-        (set, get) => ({
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+      isLoading: false,
+      error: null,
+
+      login: async (email, password) => {
+        set({ isLoading: true, error: null });
+        try {
+          const res = await api.post("/auth/login", { email, password });
+          if (res.data.success) {
+            set({
+              user: res.data.user,
+              isAuthenticated: true,
+            });
+          }
+        } catch (error) {
+          set({
+            error: error.response?.data?.message,
+          });
+        } finally {
+          set({
+            isLoading: false,
+          });
+        }
+      },
+
+      logout: async () => {
+        try {
+          await api.post("/auth/logout");
+          set({ user: null, isAuthenticated: false });
+        } catch (error) {
+          console.error("Logout error:", error);
+        }
+      },
+
+      getProfile: async () => {
+        set({ isLoading: true });
+        try {
+          const res = await api.get("/auth/profil");
+          if (res.data.success) {
+            set({ user: res.date.user, isAuthenticated: true });
+          }
+        } catch (error) {
+          set({
             user: null,
             isAuthenticated: false,
-            isLoading: false,
-            error: null,
+            error: error.response?.data?.message,
+          });
+        } finally {
+          set({ isLoading: false });
+        }
+      },
 
-            login: async (email, password) => {
-                set({ isLoading: true, error: null})
-                try {
-                    const res = await api.post('/auth/login', {email, password})
-                    if(res.data.success) {
-                        set({
-                            user: res.data.user,
-                            isAuthenticated: true
-                        })
-                    }
-                } catch (error) {
-                    set({
-                        error: error.response?.data?.message
-                        })
-                } finally {
-                    set({
-                        isLoading: false
-                    })
-                }
-            }
-        })
-    )
-)
+      clearError: () => set({ error: null }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
